0bdbb4ba738831ed0f583c6a575f25043b51e243
lujian <lujian@qlled.com>
2026-01-05 14:28:53 +0800

    ⬇️ [2026-01-05] 依赖降级: 修改为java8

--- src/main/java/com/myself/learn/algo/Main.java
+++ src/main/java/com/myself/learn/algo/Main.java
@@ -1,53 +1,46 @@
 package com.myself.learn.algo;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
 
-        Thread.Builder.OfPlatform builder = Thread.ofPlatform().name("平台线程");
+//        Thread.Builder.OfPlatform builder = Thread.ofPlatform().name("平台线程");
 //        Thread.Builder.OfVirtual builder = Thread.ofVirtual().name("虚拟线程");
-        builder.start(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    System.out.println("Main.run");
-
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            }
-        });
-
-        // 线程池
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-
-        Callable<Integer> callable = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                System.out.printf("Main.run");
-                TimeUnit.MILLISECONDS.sleep(1000);
-                return 0;
-            }
-        };
-        FutureTask<Integer> integerFutureTask = new FutureTask<>(callable);
-
-        executorService.execute(integerFutureTask);
-        Future<Integer> submit = executorService.submit(callable);
-
-        System.out.printf("main");
+//        builder.start(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true) {
+//                    System.out.println("Main.run");
+//
+//                    try {
+//                        TimeUnit.MILLISECONDS.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }
+//        });
+//
+//        // 线程池
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//
+//
+//        Callable<Integer> callable = new Callable<Integer>() {
+//            @Override
+//            public Integer call() throws Exception {
+//                System.out.printf("Main.run");
+//                TimeUnit.MILLISECONDS.sleep(1000);
+//                return 0;
+//            }
+//        };
+//        FutureTask<Integer> integerFutureTask = new FutureTask<>(callable);
+//
+//        executorService.execute(integerFutureTask);
+//        Future<Integer> submit = executorService.submit(callable);
+//
+//        System.out.printf("main");
 
     }
 }
--- src/main/java/com/myself/learn/algo/TestThread.java
+++ src/main/java/com/myself/learn/algo/TestThread.java
@@ -1,11 +1,6 @@
 package com.myself.learn.algo;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 
 /**
  * @projectName（项目名称）:qljt_cosmic
@@ -23,59 +18,59 @@ import java.util.concurrent.FutureTask;
 public class TestThread {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Runnable runnable = new Runnable() {
-            /**
-             * Runs this operation.
-             */
-            @Override
-            public void run() {
-                System.out.println("runnable");
-            }
-        };
-
-        Thread thread = new Thread(runnable);
-        thread.start();
-
-        Thread thread1 = new Thread();
-
-        Callable<Integer> callable = new Callable<Integer>() {
-            /**
-             * Computes a result, or throws an exception if unable to do so.
-             *
-             * @return computed result
-             * @throws Exception if unable to compute a result
-             */
-            @Override
-            public Integer call() throws Exception {
-                System.out.println("callable");
-                return 1;
-            }
-        };
-
-        FutureTask<Integer> futureTask = new FutureTask<>(callable);
-
-
-
-
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        executorService.submit(futureTask);
-        executorService.submit(thread1);
-        executorService.execute(runnable);
-
-        System.out.println(futureTask.get());
-
-
-        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(()->{
-            System.out.println("supplyAsync");
-            return 2;
-        }, executorService);
-        CompletableFuture<Integer> completableFuture2 = CompletableFuture.supplyAsync(()->{
-            runnable.run();
-            return 1;
-        }, executorService);
-
-
-        executorService.close();
+//        Runnable runnable = new Runnable() {
+//            /**
+//             * Runs this operation.
+//             */
+//            @Override
+//            public void run() {
+//                System.out.println("runnable");
+//            }
+//        };
+//
+//        Thread thread = new Thread(runnable);
+//        thread.start();
+//
+//        Thread thread1 = new Thread();
+//
+//        Callable<Integer> callable = new Callable<Integer>() {
+//            /**
+//             * Computes a result, or throws an exception if unable to do so.
+//             *
+//             * @return computed result
+//             * @throws Exception if unable to compute a result
+//             */
+//            @Override
+//            public Integer call() throws Exception {
+//                System.out.println("callable");
+//                return 1;
+//            }
+//        };
+//
+//        FutureTask<Integer> futureTask = new FutureTask<>(callable);
+//
+//
+//
+//
+//        ExecutorService executorService = Executors.newFixedThreadPool(3);
+//        executorService.submit(futureTask);
+//        executorService.submit(thread1);
+//        executorService.execute(runnable);
+//
+//        System.out.println(futureTask.get());
+//
+//
+//        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(()->{
+//            System.out.println("supplyAsync");
+//            return 2;
+//        }, executorService);
+//        CompletableFuture<Integer> completableFuture2 = CompletableFuture.supplyAsync(()->{
+//            runnable.run();
+//            return 1;
+//        }, executorService);
+//
+//
+//        executorService.close();
 
 //        ReentrantLock
     }
