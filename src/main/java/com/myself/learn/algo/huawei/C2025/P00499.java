23050d728d26889e724770c70ddda053c6bef458
lujian <lujian@qlled.com>
2026-01-20 09:41:29 +0800

    ✨ [2026-01-08] 添加新特性: 20260012001

---
+++ src/main/java/com/myself/learn/algo/huawei/C2025/P00499.java
@@ -0,0 +1,29 @@
+package com.myself.learn.algo.huawei.C2025;
+
+import java.util.Scanner;
+
+/**
+ * P00499.华为od机试—字符串匹配
+ * 双机位C卷
+ * difficulty:3
+ * @author luqi
+ */
+public class P00499 {
+
+    /**
+     * 全局静态 Scanner，方便在任何函数中使用
+     */
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        if (sc.hasNext()) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(n);
+        }
+        // 记得关闭（虽然机试不关也不报错，但这是好习惯）
+        sc.close();
+    }
+}
