de4c945858b3f6b9730fc061d8e927f49198a91c
lujian <fflujian@chinatelecom.cn>
2026-01-21 15:00:51 +0800

    ✨ [2026-01-21] 添加新特性:

--- src/main/java/com/myself/learn/algo/huawei/DC2025/P00481.java
+++ src/main/java/com/myself/learn/algo/huawei/DC2025/P00481.java
@@ -1,6 +1,6 @@
 package com.myself.learn.algo.huawei.DC2025;
 
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * P00481.华为od机试—最佳升级时间窗
@@ -19,9 +19,35 @@ public class P00481 {
     public static void main(String[] args) {
 
         if (sc.hasNext()) {
+            // 最佳升级时间窗口的天数
+            int maxTimes = 0;
+            int firstIndex = -1;
             int n = sc.nextInt();
-            int m = sc.nextInt();
-            System.out.println(n);
+            int[] arr = new int[168 * 2];
+            for (int i = 0; i < 168 * 2; i++) {
+                if (i < 168) {
+                    arr[i] = sc.nextInt();
+                }else {
+                    arr[i] = arr[i - 168];
+                }
+            }
+            // 滑动窗口内的访问量
+            int sum = 0;
+            int left = 0;
+            for (int right = 0; right < arr.length; right++) {
+                sum += arr[right];
+                while (sum > n && left < right) {
+                    sum -= arr[left];
+                    left++;
+                }
+                if (right - left > maxTimes) {
+                    maxTimes = right - left;
+                    firstIndex = left;
+                }
+            }
+
+
+            System.out.println(firstIndex % 168 + " " + (firstIndex + maxTimes) % 168);
         }
         // 记得关闭（虽然机试不关也不报错，但这是好习惯）
         sc.close();
--- src/main/java/com/myself/learn/algo/huawei/DC2025/P00488.java
+++ src/main/java/com/myself/learn/algo/huawei/DC2025/P00488.java
@@ -1,6 +1,7 @@
 package com.myself.learn.algo.huawei.DC2025;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * P00488.华为od机试—部门人力分配
@@ -19,11 +20,58 @@ public class P00488 {
     public static void main(String[] args) {
 
         if (sc.hasNext()) {
-            int n = sc.nextInt();
             int m = sc.nextInt();
-            System.out.println(n);
+            sc.nextLine();
+            int[] requirements = Arrays.stream(sc.nextLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(requirements);
+            // 最小月需求量
+            int minNeed = 0;
+            int left = requirements[requirements.length - 1];
+            int right = left * 2;
+
+            // 二分法
+            while (left <= right) {
+                int middle = left + (right - left) / 2;
+                boolean isPossible = checkRequirements(requirements, middle, m);
+                if (isPossible) {
+                    // 符合要求
+                    minNeed = middle;
+                    // 尝试更小的值
+                    right = middle - 1;
+                }else {
+                    left = middle + 1;
+                }
+            }
+
+            System.out.println(minNeed);
         }
         // 记得关闭（虽然机试不关也不报错，但这是好习惯）
         sc.close();
+    }
+
+    /**
+     * 检查给定的 middle 是否满足要求
+     * @param requirements 需求数组
+     * @param middle 中间值
+     * @param m 月份
+     * @return 1-need > m  -1:need < m 0 need == m
+     */
+    private static boolean checkRequirements(int[] requirements, int middle, int m) {
+        int left = 0, right = requirements.length - 1;
+        int needM = 0;
+        while (left <= right) {
+            if (requirements[left] + requirements[right] <= middle) {
+                left++;
+                right--;
+                needM ++;
+            } else {
+                right--;
+                needM ++;
+            }
+            if (needM > m) {
+                return false;
+            }
+        }
+        return true;
     }
 }
