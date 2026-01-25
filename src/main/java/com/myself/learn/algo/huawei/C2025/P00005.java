b2e94a57f49c0e25381acdd6b14b3717fdf4c6a2
lujian <lujian@qlled.com>
2026-01-25 19:31:13 +0800

    ✨ [2026-01-25] 添加新特性: TE

---
+++ src/main/java/com/myself/learn/algo/huawei/C2025/P00005.java
@@ -0,0 +1,29 @@
+package com.myself.learn.algo.huawei.C2025;
+
+import java.util.*;
+
+/**
+ * P00005.华为od机试—无向图染色
+ * 2025B卷,2025C卷
+ * difficulty:5
+ * @author luqi
+ */
+public class P00005 {
+
+    /**
+     * 全局静态 Scanner，方便在任何函数中使用
+     */
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        if (sc.hasNext()) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(n);
+        }
+        // 记得关闭（虽然机试不关也不报错，但这是好习惯）
+        sc.close();
+    }
+}
---
+++ src/main/java/com/myself/learn/algo/huawei/TE1/TE38.java
@@ -0,0 +1,29 @@
+package com.myself.learn.algo.huawei.TE1;
+
+import java.util.*;
+
+/**
+ * 38- 二分查找
+ * difficulty:简单
+ * frequency:高
+ * @author luqi
+ */
+public class TE38 {
+
+    /**
+     * 全局静态 Scanner，方便在任何函数中使用
+     */
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        if (sc.hasNext()) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(n);
+        }
+        // 记得关闭（虽然机试不关也不报错，但这是好习惯）
+        sc.close();
+    }
+}
